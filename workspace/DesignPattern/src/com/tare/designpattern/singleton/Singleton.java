362d0df51ddabbca10311696956f22de7490979f
Sean Jeong <sean.jeong@nexstreaming.com>
2016-03-03 20:05:14 +0900

    Singleton pattern added

---
+++ workspace/DesignPattern/src/com/tare/designpattern/singleton/Singleton.java
@@ -0,0 +1,23 @@
+package com.tare.designpattern.singleton;
+
+public class Singleton {
+
+	private volatile static Singleton uniqueInstance;
+
+	private Singleton() {
+
+	}
+
+	public static Singleton getInstance() {
+		if (uniqueInstance == null) {
+			synchronized (Singleton.class) {
+				if (uniqueInstance == null) {
+					uniqueInstance = new Singleton();
+				}
+			}
+		}
+
+		return uniqueInstance;
+	}
+
+}
